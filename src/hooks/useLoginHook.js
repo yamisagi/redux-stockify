4383b1419bc8ccf05954d7b6d30b0ed2ea5d5f07
Yamisagi <erencandan20@gmail.com>
2023-08-10 00:49:06 +0300

    Initial commit

---
+++ src/hooks/useLoginHook.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { toastSuccessNotify, toastErrorNotify } from '../helper/ToastNotify';
+import { useDispatch } from 'react-redux';
+import { loginSuccess } from '../features/authSlice';
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
+const useLogin = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const login = async (userData) => {
+    console.log(userData);
+    try {
+      const { data } = await axios.post(
+        `${baseUrl}/account/auth/login/`,
+        userData
+      );
+      console.log(data);
+      dispatch(loginSuccess());
+      navigate('/stock');
+      toastSuccessNotify('Login Success');
+    } catch (error) {
+      toastErrorNotify('Login Failed');
+      console.log(error);
+    }
+  };
+
+  return { login };
+};
+
+export default useLogin;
